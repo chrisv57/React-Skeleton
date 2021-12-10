f4777472813f174339513172286b55a6d40dd721
Shubam Verma <verm0101@algonquinlive.com>
2021-12-10 16:06:08 -0500

    commit

---
+++ server/express.js
@@ -0,0 +1,45 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import compress from 'compression'
+import cors from 'cors'
+import helmet from 'helmet'
+
+import Template from './../template'
+
+import userRoutes from './routes/user.routes'
+import authRoutes from './routes/auth.routes'
+
+import devBundle from './devBundle'
+
+import path from 'path'
+
+const CURRENT_WORKING_DIR = process.cwd()
+
+const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(compress())
+app.use(helmet({
+    contentSecurityPolicy: false
+}))
+app.use(cors())
+app.use('/',userRoutes)
+app.use('/',authRoutes)
+devBundle.compile(app)
+app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
+
+
+app.get('/',(req,res)=>{
+    res.status(200).send(Template())
+})
+app.use((err, req, res, next) => {
+    if (err.name === 'UnauthorizedError') {
+    res.status(401).json({"error" : err.name + ": " + err.message})
+    }else if (err) {
+    res.status(400).json({"error" : err.name + ": " + err.message})
+    console.log(err)
+    }
+    })
+export default app
